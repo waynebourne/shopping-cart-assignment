ce51b9c547532a7de48a0ae01e6b0391ac98a481
Snigdho Bhattacharya <snipap@gmail.com>
2021-03-05 12:52:53 +0530

    Updates

--- flickity.min.7476d6d5.js
+++ flickity.min.7476d6d5.js
@@ -184,7 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/main.scss":[function(require,module,exports) {
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles/utils/flickity.min.css":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/main.48b3db1d.js.map
+//# sourceMappingURL=/flickity.min.7476d6d5.js.map
--- products.d2d8c758.js
+++ products.d2d8c758.js
@@ -1557,7 +1557,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56047" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59235" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
